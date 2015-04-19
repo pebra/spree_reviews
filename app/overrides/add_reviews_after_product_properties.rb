63b305c63eaf56dea7d773d00dbfe13b8bee7fd0
pebra <self@pebra.net>
2015-04-19 12:16:58 +0200

    typo fix

--- app/overrides/add_reviews_after_product_properties.rb
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'converted_product_properties_767643482',
   #insert_after: '[data-hook="product_properties"]',
-  insert_after: '[data-hook="product_right_part_wrap"],
+  insert_after: '[data-hook="product_right_part_wrap"]',
   partial: 'spree/shared/reviews'
 )
